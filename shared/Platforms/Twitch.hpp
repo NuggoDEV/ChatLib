50e7d5d87838e4fd1ffb84d3ae0c6679e57c4423
hdgamer1404Jonas <hdgamer1404Jonas@gmail.com>
2023-10-13 18:30:18 +0200

    Move connect methods into private files

--- shared/Platforms/Twitch.hpp
+++ shared/Platforms/Twitch.hpp
@@ -2,21 +2,13 @@
 
 #include <string>
 #include <vector>
+
 #include "Objects/Twitch/Message.hpp"
+#include "Objects/Twitch/User.hpp"
+#include "Objects/Twitch/Client.hpp"
 
 namespace Twitch::Connection {
-    std::string username = nullptr;
-    std::string token = nullptr;
-    std::string channel = nullptr;
-    bool connected = false;
-
-    ///@brief Connect as a user (Client) to a twitch channel
-    ///@tparam username Name of the Client
-    ///@tparam token Auth Token of the Client
-    ///@tparam channel Name of the Channel to listen to
-    void connect(std::string username, std::string token, std::string channel); // Connects / Reconnects with the given account to the given channel
-    ///@brief Disconnects the Client
-    void disconnect();
     ///@brief Get information about the client
-    void getUser();
+    ///@return Twitch::Client
+    void getClient();
 }
--- src/Platforms/Twitch.cpp
+++ src/Platforms/Twitch.cpp
@@ -1,13 +1,7 @@
 #include "Platforms/Twitch.hpp"
 
 namespace Twitch::Connection {
-    void connect(std::string username, std::string token, std::string channel) {
-
-    }
-    void disconnect() {
-
-    }
     void getUser() {
-
+        
     }
 }
