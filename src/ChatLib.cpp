740dcd48ec5ae7150ae948b4564bc05554a537d6
Nuggo <nuggodev@gmail.com>
2023-10-30 20:33:31 +1030

    Use defines, more UI stuff

--- src/ChatLib.cpp
+++ src/ChatLib.cpp
@@ -1,19 +1,21 @@
 #include "ChatLib.hpp"
 #include "Logger.hpp"
 #include "WebServer/WebServer.hpp"
+#include "UI/CLView.hpp"
+#include "bsml/shared/BSML.hpp"
 
 bool didInit = false;
 
 Logger& getLogger()
 {
-    static auto logger = new Logger(ModInfo{"ChatLib", "0.0.1"}, LoggerOptions(false, true));
+    static auto logger = new Logger(ModInfo{MOD_ID, VERSION}, LoggerOptions(false, true));
     return *logger;
 }
 
 extern "C" void setup(ModInfo& info)
 {
-    info.id = "ChatLib";
-    info.version = "0.0.1";
+    info.id = MOD_ID;
+    info.version = VERSION;
 }
 
 extern "C" void load()
@@ -25,11 +27,10 @@ void ChatLib::Init()
 {
     if (didInit)
         return;
-
-    WebServer::start();
-
     didInit = true;
-    getLogger().info("Started initializing");
-
     il2cpp_functions::Init();
+
+    getLogger().info("Started initializing");
+    WebServer::start();
+    BSML::Register::RegisterSettingsMenu<ChatLib::UI::CLView*>("ChatLib", false);
 } 
