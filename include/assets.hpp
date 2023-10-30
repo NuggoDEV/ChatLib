418e6c8d207bd59ab251be7e59290d4058d85c5a
Nuggo <nuggodev@gmail.com>
2023-10-30 20:33:35 +1030

    Misc

--- include/assets.hpp
+++ include/assets.hpp
@@ -34,7 +34,9 @@ struct IncludedAsset {
     const IncludedAsset name { _binary_##name##_start, _binary_##name##_end};
 namespace IncludedAssets {
 
+	DECLARE_FILE(emptypage_html)
 	DECLARE_FILE(header_background_jpg)
 	DECLARE_FILE(index_html)
+	DECLARE_FILE(main_bsml)
 
 }
