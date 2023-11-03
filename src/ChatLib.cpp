df983beb086a45e92104f615e8c3b1cc5551558d
Nuggo <nuggodev@gmail.com>
2023-11-03 18:19:13 +1030

    Config (not done)

--- src/ChatLib.cpp
+++ src/ChatLib.cpp
@@ -3,12 +3,14 @@
 #include "WebServer/WebServer.hpp"
 #include "UI/CLView.hpp"
 #include "bsml/shared/BSML.hpp"
+#include "Config.hpp"
 
+ModInfo modInfo = ModInfo{MOD_ID, VERSION};
 bool didInit = false;
 
 Logger& getLogger()
 {
-    static auto logger = new Logger(ModInfo{MOD_ID, VERSION}, LoggerOptions(false, true));
+    static auto logger = new Logger(modInfo, LoggerOptions(false, true));
     return *logger;
 }
 
@@ -31,6 +33,7 @@ void ChatLib::Init()
     il2cpp_functions::Init();
 
     getLogger().info("Started initializing");
+    getConfig().Init(modInfo);
     WebServer::start();
     BSML::Register::RegisterSettingsMenu<ChatLib::UI::CLView*>("ChatLib", false);
 } 
