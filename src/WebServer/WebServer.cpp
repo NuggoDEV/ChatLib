d7a8cf39e391a1d271d32209d97c1cc403c912db
Nuggo <nuggodev@gmail.com>
2023-11-07 22:54:57 +1030

    Remove /auth, add token using SafeStorage

--- src/WebServer/WebServer.cpp
+++ src/WebServer/WebServer.cpp
@@ -1,7 +1,9 @@
 #include "WebServer/WebServer.hpp"
 #include "assets.hpp"
 #include "WebServer/ServeAssets.hpp"
-#include "PrivateVariables.hpp"
+#include "Logger.hpp"
+
+#include "safestorage/shared/interface.hpp"
 
 std::string code = "";
 
@@ -17,17 +19,15 @@ namespace WebServer {
             server.Get("/", [](const httplib::Request& req, httplib::Response& res) {
                 const std::string_view htmlContent = static_cast<std::string_view>(IncludedAssets::index_html);
                 res.set_content(htmlContent.data(), htmlContent.size(), "text/html");
+
+                if (req.has_param("token"))
+                {
+                    SafeStorage::Interface::remove(GetModInfo(), "Token");
+                    SafeStorage::Interface::insert(GetModInfo(), "Token", req.get_param_value("Token"));
+                }
             });
 
             serveAssets(server);
-
-            server.Get("/auth", [](const httplib::Request& req, httplib::Response& res) {
-                if (req.has_param("token"))
-                {
-                    code = req.get_param_value("token");
-                    res.set_content(code.data(), code.size(), "text/plain");
-                }
-            });
 
             server.listen("0.0.0.0", 50009);
         });
