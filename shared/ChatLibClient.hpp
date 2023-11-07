9ee509ebcaf3548b89be7e2c40f947bb041e8d5f
Nuggo <nuggodev@gmail.com>
2023-11-07 22:50:50 +1030

    Add functions, very not finished

--- shared/ChatLibClient.hpp
+++ shared/ChatLibClient.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Services/IRC/TwitchIRCSocket.hpp"
 #include <string>
 
 namespace ChatLib
@@ -7,7 +8,25 @@ namespace ChatLib
     class ChatLibClient
     {
         public:
-        void Connect(std::string channel);
-        void Disconnect();
+        const char *address = "irc.chat.twitch.tv";
+        const int port = 6667;
+
+        //bool CreateClient();
+        int CreateSocketAndConnect();
+        bool AuthenticateWithTwitch(int sockfd, const char* accessToken, const char* username);
+        void JoinChannel(int sockfd, const char* channel);
+        void ReceiveAndPrintMessages(int sockfd);
+
+        //bool Login();
+        //void Connect(std::string channel);
+        //void Disconnect();
+        
+        //bool SendData(std::string data);
+        //bool IsTokenValid(std::string token);
+
+        private:
+        int _socket;
+        bool _connected = false;
+        //Services::TwitchIRCSocket socket;
     };
 }
