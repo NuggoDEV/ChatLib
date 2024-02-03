cd810e873b25b75f6146c8d607eb59494ef3f7b0
Jonas W <hdgamer1404Jonas@gmail.com>
2024-02-03 18:34:13 +0100

    Global Message handling

---
+++ include/Handler.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+
+#include "Types/Global/Message.hpp"
+
+namespace ChatLib::Handler {
+    std::vector<std::function<void(ChatLib::Types::Global::Message)>> globalCallbacks;
+
+    void addGlobalCallback(std::function<void(ChatLib::Types::Global::Message)> callback) {
+        globalCallbacks.push_back(callback);
+    }
+
+    void handleGlobalMessage(ChatLib::Types::Global::Message message) {
+        for (auto callback : globalCallbacks) {
+            callback(message);
+        }
+    }
+
+    
+}
